59999072f6275e2e17d504f7107f82569373d425
unknown <jhhegna@BV0158848.EMP.BV>
2012-10-09 12:21:51 -0500

    10/9/12 added batch files to clear bugs

---
+++ Reset_settings.bat
@@ -0,0 +1,6 @@
+mkdir "bin/settings"
+echo high > bin/settings/graphics
+echo medium > bin/settings/difficulty
+echo no > bin/settings/development-mode
+echo 0 > bin/settings/13centurycalamity
+echo 1 > bin/settings/saveGame
